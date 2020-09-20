adf51bf305b380ed02c8194a7131499ff9ce4d6b
George Armando <georgearmando54@gmail.com>
2020-09-20 18:46:53 +0100

    Arquivos modificados

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,17 +1,28 @@
 import { uuid } from 'uuidv4';
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import User from './Users';
 
 @Entity('appointments')
 class Appointment {
 
   @PrimaryGeneratedColumn('uuid')
-  id: string;
+  id: 'string';
 
   @Column()
-  provider: string;
+  provider_id: String;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 export default Appointment;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,38 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
+import uploadConfig from './config/upload';
+import AppError from './errors/AppErrors';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory))// Rota para visualizar arquivos estáticos (imagens)
 app.use(routes);
+
+// Tratamento de ERROS
+// Fica depois de todas as rotas pk é de lá onde são gerados os erros
+app.use(
+  (err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('Server running on port 3333!');
